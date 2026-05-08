674207afc81aa9c8e0a051fe490a94506dd1699b
Haider Baig <haider.baig@tkxel.io>
2026-05-08 18:39:15 +0500

    better UI

--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
 export default function Square({ value, onClick }) {
-
   return (
-    <button className="square" onClick={onClick}>
+    <button
+      className={`square ${value === "X" ? "square-x" : value === "O" ? "square-o" : ""}`}
+      onClick={onClick}
+    >
       {value}
     </button>
   );
